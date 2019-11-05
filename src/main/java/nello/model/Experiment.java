606f62ad4c298cf5d3fd3ea93a39d10d36d699bd
Insomnia-2408 <s1113523@student.hsleiden.nl>
2019-11-05 21:51:16 +0100

    Completed observer and observable, added logic for creating an experiment

--- src/main/java/nello/model/Experiment.java
+++ src/main/java/nello/model/Experiment.java
@@ -1,11 +1,16 @@
 package nello.model;
 
 import com.sun.istack.internal.NotNull;
+import nello.observable.ExperimentObservable;
+import nello.observer.ExperimentObserver;
 
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Experiment {
+public class Experiment implements ExperimentObservable {
 
     public enum Category {
         INWERKING(1), AFGEROND(2), VASTEDIENST(3);
@@ -55,22 +60,24 @@ public class Experiment {
 
     }
 
-    public long id;
-    public Category category;
-    public Phase phase;
-    public String businessOwner;
-    public String description;
-    public String name;
-    public String statusColor;
-    public Date createDate;
-    public Date endDate;
-    public String status;
-    public ArrayList<Log> logs;
-    public ArrayList<Attachment> attachments;
-    public boolean isLocked;
+    private long id;
+    private Category category;
+    private Phase phase;
+    private String businessOwner;
+    private String description;
+    private String name;
+    private String statusColor;
+    private LocalDate createDate;
+    private Date endDate;
+    private String status;
+    private List<Log> logs;
+    private List<Attachment> attachments;
+    private boolean isLocked;
+
+    private List<ExperimentObserver> observerList;
 
     public Experiment(long id, Category category, Phase phase, String businessOwner, String description, String name,
-                      String statusColor, Date createDate, String status, ArrayList<Log> logs,
+                      String statusColor, LocalDate createDate, String status, ArrayList<Log> logs,
                       ArrayList<Attachment> attachments, boolean isLocked) {
 
         this.id = id;
@@ -89,41 +96,90 @@ public class Experiment {
 
     }
 
+    @Override
+    public void notifyObservers() {
+        for (ExperimentObserver o : observerList) {
+            o.update(this);
+        }
+    }
+
+    @Override
+    public void registerObserver(ExperimentObserver observer) {
+        // register observer
+        observerList.add(observer);
+
+        // notify him on latest updates
+        observer.update(this);
+    }
+
     public long getId() {return id;}
-    public void setId(long id) {this.id = id;}
 
     public Category getCategory() {return category;}
-    public void setCategory(Category category) {this.category = category;}
+    public void setCategory(Category category) {
+        this.category = category;
+        notifyObservers();
+    }
 
     public Phase getPhase() {return phase;};
-    public void setPhase(Phase phase) {this.phase = phase;}
+    public void setPhase(Phase phase) {
+        this.phase = phase;
+        notifyObservers();
+    }
 
     public String getBusinessOwner() {return businessOwner;}
-    public void setBusinessOwner(String businessOwner) {this.businessOwner = businessOwner;}
+    public void setBusinessOwner(String businessOwner) {
+        this.businessOwner = businessOwner;
+        notifyObservers();
+    }
 
     public String getDescription() {return description;}
-    public void setDescription(String description) {this.description = description;}
+    public void setDescription(String description) {
+        this.description = description;
+        notifyObservers();
+    }
 
     public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    public void setName(String name) {
+        this.name = name;
+        notifyObservers();
+    }
 
     public String getStatusColor() {return statusColor;}
-    public void setStatusColor(String statusColor) {this.statusColor = statusColor;}
+    public void setStatusColor(String statusColor) {
+        this.statusColor = statusColor;
+        notifyObservers();
+    }
 
-    public Date getCreateDate() {return createDate;}
-    public void setCreateDate(Date createDate) {this.createDate = createDate;}
+    public LocalDate getCreateDate() {return createDate;}
 
     public Date getEndDate() {return endDate;}
-    public void setEndDate(Date endDate) {this.endDate = endDate;}
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+        notifyObservers();
+    }
 
     public String getStatus() {return status;}
-    public void setStatus(String status) {this.status = status;}
+    public void setStatus(String status) {
+        this.status = status;
+        notifyObservers();
+    }
 
-    //TODO: fix
-//    public String getLog() {return log;}
-//    public void setLog(String log) {this.log = log;}
+    public List<Log> getLogs() {return logs;}
+    public void setLogs(List<Log> logs) {
+        this.logs = logs;
+        notifyObservers();
+    }
+
+    public List<Attachment> getAttachments() {return attachments;}
+    public void setAttachments(List<Attachment> attachments) {
+        this.attachments = attachments;
+        notifyObservers();
+    }
 
     public boolean isLocked() {return isLocked;}
-    public void setLocked(boolean isLocked) {this.isLocked = isLocked;}
+    public void setLocked(boolean isLocked) {
+        this.isLocked = isLocked;
+        notifyObservers();
+    }
 
 }
