31595a1031a113ecb28c8f6420cc40bf105a1c4a
Sanne <SanneSVisser@gmail.com>
2019-11-07 15:59:17 +0100

    Using FileChooser

--- src/main/java/nello/controller/AttachmentController.java
+++ src/main/java/nello/controller/AttachmentController.java
@@ -1,4 +1,11 @@
 package nello.controller;
 
 public class AttachmentController implements IController {
+
+    public pickAttachment(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showOpenDialog(stage);
+    }
+
 }
