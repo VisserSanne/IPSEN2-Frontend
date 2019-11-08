a1ca8af1146ec0fc52950d50bbc0e815c866daa3
Sanne <SanneSVisser@gmail.com>
2019-11-08 17:10:19 +0100

    Added the logic

--- src/main/java/nello/controller/AttachmentController.java
+++ src/main/java/nello/controller/AttachmentController.java
@@ -1,33 +1,48 @@
 package nello.controller;
 
-import javafx.stage.FileChooser;
-
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
 
 public class AttachmentController implements IController {
 
-    private final Desktop desktop = Desktop.getDesktop();
-
-    public void pickAttachment(){
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Choose File");
+    public void pickAttachment() {
+        JFileChooser fileChooser = new JFileChooser();
+        configureFileChooser(fileChooser);
+        //Todo: add stage
         File file = fileChooser.showOpenDialog(stage);
         if (file != null) {
-            openFile(file);
+            File chosenFile = fileChooser.getSelectedFile();
+            setInDb(chosenFile);
         }
+
+
+    }
+
+    private void configureFileChooser(JFileChooser fileChooser) {
+        fileChooser.setDialogTitle("Choose File");
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Textual documents and images", "doc", "txt", "jpg", "png");
+        fileChooser.setFileFilter(filter);
     }
 
 
-    private void openFile(File file) {
-        EventQueue.invokeLater(() -> {
-            try {
-                desktop.open(file);
-            } catch (IOException e) {
-                e.getStackTrace();
-            }
-        });
-    }
+    private byte[] setInDb(File file) {
+        try {
+            FileInputStream inputStream = new FileInputStream(file);
+            int fileLength = (int) file.length();
+            byte[] incoming_file_data = new byte[fileLength]; // allocate byte array of right size
+            inputStream.read(incoming_file_data, 0, fileLength); // read into byte array
+            inputStream.close();
+            return incoming_file_data;
+            //Todo: to db
+        } catch (Exception err) {
+            err.printStackTrace();
+            return null;
+        }
 
+    }
 }
